7079ffcd27730159b905d1a1b7017247b82a4a25
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-09 10:57:34 +0200

    Added index.ts/js/js.map

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const redux_1 = require("redux");
 const store = (0, redux_1.createStore)(() => { });
 console.log(store);
-//# sourceMappingURL=main.js.map
+//# sourceMappingURL=index.js.map
