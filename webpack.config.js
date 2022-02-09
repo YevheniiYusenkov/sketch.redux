826564410b6d65a3297e45b4975db66c4d63c5e3
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-09 11:24:17 +0200

    Added HtmlWebpackPlugin to webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,21 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const stylesHandler = 'style-loader';
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
   },
   devServer: {
     open: true,
     host: 'localhost',
   },
-  plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
+  plugins: [new HtmlWebpackPlugin({
+    template: 'index.html'
+  })],
   module: {
     rules: [
       {
@@ -34,9 +36,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
