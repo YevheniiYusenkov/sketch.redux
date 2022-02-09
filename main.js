21ae74d084898c4ce7b036004be9cca6e554e9db
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-09 02:19:53 +0200

    Added js/js.map for main.ts

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const redux_1 = require("redux");
+const store = (0, redux_1.createStore)(() => { });
+console.log(store);
+//# sourceMappingURL=main.js.map
